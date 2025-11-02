c45d28257c09d6861f2f321e65bdb0813266c903
hkolukul <69184958+hkolukul@users.noreply.github.com>
2025-11-02 19:19:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+    //My first JS code
+    console.log('Good Morning Guys!');
+    let myName = 'Hari';
+    console.log(myName);
+
+    //variable can not start with number, should not contain space or - special characters
+    //variables are case sensitive
+    let firstName = 'Hari';
+    let last_name = 'Kolukuluri';
+    console.log(firstName + ' ' + last_name);
+
+    //Data Types: string, number, boolean, undefined, null
+
